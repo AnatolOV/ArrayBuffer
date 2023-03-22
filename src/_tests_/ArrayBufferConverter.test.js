2a025e22fc86363cb0191d4daa61a497029a2cb1
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2023-03-22 20:42:52 +0300

    1

---
+++ src/_tests_/ArrayBufferConverter.test.js
@@ -0,0 +1,18 @@
+import ArrayBufferConverter from '../ArrayBufferConverter';
+import getBuffer from '../index';
+
+test('Проверка загрузки данных.', () => {
+  const input = new ArrayBufferConverter();
+  input.load(getBuffer());
+
+  expect(input.buffer).toEqual(getBuffer());
+});
+
+test('Проверка перевода загруженных данных в строку.', () => {
+  const input = new ArrayBufferConverter();
+  input.load(getBuffer());
+
+  expect(input.toString()).toBe(
+    '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}',
+  );
+});
